3cce386da28bfda181acca91600c2e3d33469140
Pablo Rodriguez Guillamon <40163073+parogui@users.noreply.github.com>
2019-10-29 12:10:16 -0400

    Update display.sh

--- resources/scripts/deploy/display.sh
+++ resources/scripts/deploy/display.sh
@@ -3,7 +3,7 @@ echo "Updated Deployment.json"
 
 cat deployment.json
 #if i uncomment the following command, above cat is working, if i comment (hence cat is the last command in the script) then its not working, weird
-printf "\n"
-#echo "Cat is working"
+#printf "\n"
+echo "Cat is working"
 
 
